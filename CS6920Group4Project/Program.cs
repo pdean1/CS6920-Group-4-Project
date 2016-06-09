a418724db9d8297b4bf2553c2f9e83d2ba783d47
pdean1 <pdean1@my.westga.edu>
2016-06-09 19:01:07 -0400

    Adding a stub for the login for so that we can see visual progress

--- CS6920Group4Project/Program.cs
+++ CS6920Group4Project/Program.cs
@@ -16,7 +16,7 @@ namespace CS6920Group4Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new forms.LoginForm());
         }
     }
 }
---
+++ CS6920Group4Project/forms/LoginForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS6920Group4Project.forms
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            InitializePasswordBox();
+        }
+
+        private void InitializePasswordBox()
+        {
+            tbPassword.PasswordChar = '*';
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            String username = tbUserName.Text;
+            String password = tbPassword.Text;
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please provide a username and password");
+                tbPassword.Clear();
+                return;
+            }
+            MessageBox.Show("Login not yet implemented");
+        }
+    }
+}
