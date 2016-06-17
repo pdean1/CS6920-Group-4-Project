5f1f85eab02fdf11fc41d5e86015f627fa942c46
pdean1 <pdean1@my.westga.edu>
2016-06-17 00:42:01 -0400

    Had to redo the test project

--- CS6920Group4ProjectTests/Properties/AssemblyInfo.cs
+++ CS6920Group4ProjectTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("CS6920Group4ProjectTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("CS6920Group4ProjectTests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d98972d4-3508-4b30-b864-826df4b19ad0")]
+[assembly: Guid("d21d39ff-697e-474b-af50-0bc061187c04")]
 
 // Version information for an assembly consists of the following four values:
 //
