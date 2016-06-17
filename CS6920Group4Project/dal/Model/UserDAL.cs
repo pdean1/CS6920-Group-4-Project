1225c4d2ebaf38cf8995033bc9bd38a1941bf1a8
pdean1 <pdean1@my.westga.edu>
2016-06-17 01:56:17 -0400

    Created a UserDAL NOT COMPLETE YET

---
+++ CS6920Group4Project/dal/Model/UserDAL.cs
@@ -0,0 +1,35 @@
+﻿using CS6920Group4Project.dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using CS6920Group4Project.model;
+namespace CS6920Group4Project.DAL.Model
+{
+    public class UserDAL
+    {
+        /// <summary>
+        /// This is the connection to open
+        /// </summary>
+        private MySqlConnection conn = new DBConnect().GetConnection();
+
+        private const String InsertUserStatement = "INSERT INTO `sql5123046`.`users` (`Email`, `Password`, `DateCreated`)" + 
+            " VALUES (@Email, @Password, @DateCreated);";
+
+        public long InsertUser(User user)
+        {
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = conn;
+            command.CommandText = InsertUserStatement;
+            command.Prepare();
+            command.Parameters.AddWithValue("@Email", user.Email);
+            command.Parameters.AddWithValue("@Password", user.Password);
+            command.Parameters.AddWithValue("@DateCreated", String.Format("yyyy-MM-dd", DateTime.Now));
+            command.ExecuteNonQuery();
+            return command.LastInsertedId;
+        }
+    }
+}
