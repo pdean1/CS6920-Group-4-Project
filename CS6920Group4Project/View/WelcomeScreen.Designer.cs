0e9e7c591999c72b91868f84dabca0a432922533
twilli58 <twilli58@my.westga.edu>
2016-06-19 01:28:21 -0400

    Deleted login and created new login on welcome screen per new designs

--- CS6920Group4Project/View/WelcomeScreen.Designer.cs
+++ CS6920Group4Project/View/WelcomeScreen.Designer.cs
@@ -29,7 +29,44 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WelcomeScreen));
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.llCreateAccount = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(385, 472);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(284, 20);
+            this.tbUserName.TabIndex = 2;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(816, 472);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(276, 20);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(658, 552);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(103, 34);
+            this.btnLogIn.TabIndex = 4;
+            this.btnLogIn.Text = "Submit";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // llCreateAccount
+            // 
+            this.llCreateAccount.AutoSize = true;
+            this.llCreateAccount.Location = new System.Drawing.Point(626, 612);
+            this.llCreateAccount.Name = "llCreateAccount";
+            this.llCreateAccount.Size = new System.Drawing.Size(172, 13);
+            this.llCreateAccount.TabIndex = 5;
+            this.llCreateAccount.TabStop = true;
+            this.llCreateAccount.Text = "New User? Create a new Account.";
             // 
             // WelcomeScreen
             // 
@@ -38,13 +75,24 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1384, 762);
+            this.Controls.Add(this.llCreateAccount);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserName);
             this.DoubleBuffered = true;
             this.Name = "WelcomeScreen";
             this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.WelcomeScreen_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.LinkLabel llCreateAccount;
     }
 }
--- CS6920Group4Project/View/WelcomeScreen.cs
+++ CS6920Group4Project/View/WelcomeScreen.cs
@@ -15,6 +15,50 @@ namespace CS6920Group4Project.View
         public WelcomeScreen()
         {
             InitializeComponent();
+            InitializePasswordBox();
         }
+
+        private void InitializePasswordBox()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WelcomeScreen_Load(object sender, EventArgs e)
+        {
+            tbPassword.PasswordChar = '*';
+        }
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            String username = tbUserName.Text;
+           // String password = tbPassword.Text;
+           // if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please provide a username and password");
+                tbPassword.Clear();
+                return;
+            }
+                MessageBox.Show("Login not yet implemented");
+        }
+
+        private void llCreateAccount_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Create User not yet implemented");
+            Form cuForm = new CreateUserForm();
+            cuForm.FormClosed += new FormClosedEventHandler(otherForm_FormClosed);
+            this.Hide();
+            cuForm.Show();
+        }
+
+
+        void otherForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
