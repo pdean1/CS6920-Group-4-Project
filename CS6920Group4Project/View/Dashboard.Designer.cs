6ec595393ff428fb02f7d0082599496ad39abad4
twilli58 <twilli58@my.westga.edu>
2016-06-19 02:51:49 -0400

    Added dashboard form and begun initial designs per mockups

---
+++ CS6920Group4Project/View/Dashboard.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CS6920Group4Project.View
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.HeaderPnl = new System.Windows.Forms.Panel();
+            this.tipsPnl = new System.Windows.Forms.Panel();
+            this.dashboardpnl = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.welcomeLbl = new System.Windows.Forms.Label();
+            this.balanceLbl = new System.Windows.Forms.Label();
+            this.percentageCht = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.HeaderPnl.SuspendLayout();
+            this.dashboardpnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.percentageCht)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // HeaderPnl
+            // 
+            this.HeaderPnl.BackColor = System.Drawing.Color.Transparent;
+            this.HeaderPnl.Controls.Add(this.balanceLbl);
+            this.HeaderPnl.Controls.Add(this.welcomeLbl);
+            this.HeaderPnl.Location = new System.Drawing.Point(22, 1);
+            this.HeaderPnl.Name = "HeaderPnl";
+            this.HeaderPnl.Size = new System.Drawing.Size(1338, 188);
+            this.HeaderPnl.TabIndex = 0;
+            // 
+            // tipsPnl
+            // 
+            this.tipsPnl.BackColor = System.Drawing.Color.Transparent;
+            this.tipsPnl.Location = new System.Drawing.Point(22, 195);
+            this.tipsPnl.Name = "tipsPnl";
+            this.tipsPnl.Size = new System.Drawing.Size(182, 555);
+            this.tipsPnl.TabIndex = 1;
+            // 
+            // dashboardpnl
+            // 
+            this.dashboardpnl.BackColor = System.Drawing.Color.White;
+            this.dashboardpnl.Controls.Add(this.percentageCht);
+            this.dashboardpnl.Location = new System.Drawing.Point(211, 195);
+            this.dashboardpnl.Name = "dashboardpnl";
+            this.dashboardpnl.Size = new System.Drawing.Size(930, 555);
+            this.dashboardpnl.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Location = new System.Drawing.Point(1148, 195);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(212, 555);
+            this.panel3.TabIndex = 3;
+            // 
+            // welcomeLbl
+            // 
+            this.welcomeLbl.AutoSize = true;
+            this.welcomeLbl.BackColor = System.Drawing.Color.Transparent;
+            this.welcomeLbl.Font = new System.Drawing.Font("Calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLbl.ForeColor = System.Drawing.Color.DarkGreen;
+            this.welcomeLbl.Location = new System.Drawing.Point(141, 52);
+            this.welcomeLbl.Name = "welcomeLbl";
+            this.welcomeLbl.Size = new System.Drawing.Size(79, 22);
+            this.welcomeLbl.TabIndex = 7;
+            this.welcomeLbl.Text = "Welcome";
+            this.welcomeLbl.Click += new System.EventHandler(this.welcomeLbl_Click);
+            // 
+            // balanceLbl
+            // 
+            this.balanceLbl.AutoSize = true;
+            this.balanceLbl.BackColor = System.Drawing.Color.Transparent;
+            this.balanceLbl.Font = new System.Drawing.Font("Calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.balanceLbl.ForeColor = System.Drawing.Color.DarkGreen;
+            this.balanceLbl.Location = new System.Drawing.Point(540, 52);
+            this.balanceLbl.Name = "balanceLbl";
+            this.balanceLbl.Size = new System.Drawing.Size(183, 22);
+            this.balanceLbl.TabIndex = 8;
+            this.balanceLbl.Text = "Your Current balance is:";
+            // 
+            // percentageCht
+            // 
+            this.percentageCht.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Emboss;
+            chartArea1.Name = "ChartArea1";
+            this.percentageCht.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.percentageCht.Legends.Add(legend1);
+            this.percentageCht.Location = new System.Drawing.Point(333, 281);
+            this.percentageCht.Name = "percentageCht";
+            this.percentageCht.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.percentageCht.Series.Add(series1);
+            this.percentageCht.Size = new System.Drawing.Size(300, 300);
+            this.percentageCht.TabIndex = 0;
+            this.percentageCht.Text = "Budget";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1384, 762);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.dashboardpnl);
+            this.Controls.Add(this.tipsPnl);
+            this.Controls.Add(this.HeaderPnl);
+            this.DoubleBuffered = true;
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.HeaderPnl.ResumeLayout(false);
+            this.HeaderPnl.PerformLayout();
+            this.dashboardpnl.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.percentageCht)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel HeaderPnl;
+        private System.Windows.Forms.Panel tipsPnl;
+        private System.Windows.Forms.Panel dashboardpnl;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label welcomeLbl;
+        private System.Windows.Forms.Label balanceLbl;
+        private System.Windows.Forms.DataVisualization.Charting.Chart percentageCht;
+    }
+}
---
+++ CS6920Group4Project/View/Dashboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS6920Group4Project.View
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void welcomeLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
