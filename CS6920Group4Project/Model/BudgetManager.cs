5acce3f5993c8f4e4f97a6bf7e51679596c357f6
pdean1 <pdean1@my.westga.edu>
2016-06-21 23:40:36 -0400

    Adding the budget manager class to the project

---
+++ CS6920Group4Project/Model/BudgetManager.cs
@@ -0,0 +1,16 @@
+﻿using CS6920Group4Project.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS6920Group4Project.Model
+{
+    public class BudgetManager
+    {
+        public List<Bill> bills { get; set; }
+        public List<Earning> earnings { get; set; }
+        public List<Expense> expenses { get; set; }
+    }
+}
