7cb3038d7319696cc4acc10488e8c56d3b6e9491
pdean1 <pdean1@my.westga.edu>
2016-06-21 23:40:02 -0400

    Adding the user controller to the project

---
+++ CS6920Group4Project/Controller/UserController.cs
@@ -0,0 +1,54 @@
+﻿using CS6920Group4Project.DAL.Model;
+using CS6920Group4Project.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS6920Group4Project.Controller
+{
+    /// <summary>
+    /// This class controls access to the model classes and the database
+    /// </summary>
+    public class UserController : User
+    {
+        static readonly UserController instance = new UserController();
+        
+        private readonly UserDAL userDAL = new UserDAL();
+
+        public static UserController Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Inserts a user, u, into the database
+        /// </summary>
+        /// <param name="u">User to insert</param>
+        /// <returns>The id of the inserted record, or false</returns>
+        public long InsertUser(User u)
+        {
+            return userDAL.InsertUser(u);
+        }
+
+        /// <summary>
+        /// Attempts to log the user in. If successful the Session is set
+        /// </summary>
+        /// <param name="username">Username of the user to log in</param>
+        /// <param name="password">the password of the user to log in</param>
+        /// <returns>True if session was set false otherwise</returns>
+        public Boolean Login(string username, string password)
+        {
+            User u = new User();
+            u = userDAL.AttemptUserLogin(username, password);
+            if (u == null)
+                return false;
+            Session.SessionInformation.SetSessionUser(u);
+            return true;
+        }
+    }
+}
