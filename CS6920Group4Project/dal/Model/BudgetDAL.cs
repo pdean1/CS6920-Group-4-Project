921ef95fc252476332bb60db4089c76795860cea
pdean1 <pdean1@my.westga.edu>
2016-06-22 00:02:23 -0400

    Budget DAL Class init commit

---
+++ CS6920Group4Project/dal/Model/BudgetDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using CS6920Group4Project.dal;
+using CS6920Group4Project.model;
+
+namespace CS6920Group4Project.DAL.Model
+{
+    public class BudgetDAL
+    {
+        private MySqlConnection conn = new DBConnect().GetConnection();
+
+        private const string InsertBudgetStatement = "";
+
+        public long InsertBudget(Budget budget)
+        {
+            return 0;
+        }
+
+    }
+}
