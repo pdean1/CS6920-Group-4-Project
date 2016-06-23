1ed1ac3b294046b0a993d0623851e4f12de1a3ea
twilli58 <twilli58@my.westga.edu>
2016-06-23 15:53:33 -0400

    created the DAL for manage earnings class

---
+++ CS6920Group4Project/dal/Model/ManageEarningsDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using CS6920Group4Project.Model;
+using CS6920Group4Project.DAL.Model;
+using System.Windows.Forms;
+using CS6920Group4Project.Utilities;
+
+namespace CS6920Group4Project.DAL.Model
+{
+    class ManageEarningsDAL
+    {
+        /// <summary>
+        /// connector for the manage earnings 
+        /// </summary>
+        private MySqlConnection conn = new DBConnect().GetConnection();
+
+    }
+}
