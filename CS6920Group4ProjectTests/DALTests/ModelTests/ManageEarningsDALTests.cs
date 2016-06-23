fa3b1eb70d2373e1053574223d0218a8b6a30fe9
twilli58 <twilli58@my.westga.edu>
2016-06-23 17:05:48 -0400

    pushing fixes to previous added files

--- CS6920Group4ProjectTests/DALTests/ModelTests/ManageEarningsDALTests.cs
+++ CS6920Group4ProjectTests/DALTests/ModelTests/ManageEarningsDALTests.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CS6920Group4Project.DAL.Model;
 using CS6920Group4Project.Model;
-using System.Collections.Generic;
+
 
 
 namespace CS6920Group4ProjectTests.DALTests.ModelTests
