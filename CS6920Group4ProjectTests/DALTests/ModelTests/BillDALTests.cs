5bdc81cc5e272ede8e1c0ca1505b79d8b391cf30
pdean1 <pdean1@my.westga.edu>
2016-06-22 22:48:07 -0400

    Added DAL files for Bills, Earnings, and Expenses along with test files for them

---
+++ CS6920Group4ProjectTests/DALTests/ModelTests/BillDALTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CS6920Group4ProjectTests.DALTests.ModelTests
+{
+    [TestClass]
+    public class BillDALTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
