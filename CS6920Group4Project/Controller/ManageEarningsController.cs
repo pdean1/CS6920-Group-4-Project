22203b54cb95fe67e838c7d9329787724cbc5d27
twilli58 <twilli58@my.westga.edu>
2016-06-23 16:23:50 -0400

    added the controller methods for the dal inserts

--- CS6920Group4Project/Controller/ManageEarningsController.cs
+++ CS6920Group4Project/Controller/ManageEarningsController.cs
@@ -18,5 +18,28 @@ namespace CS6920Group4Project.Controller
         {
 
         }
+
+        public static ManageEarningsController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ManageEarningsController();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Inserts a earning, earn, into the database
+        /// </summary>
+        /// <param name="earning">inserts earnings</param>
+        /// <param name="category">inserts category</param>
+        /// <returns>the inserted record or false</returns>
+        public Boolean InsertEarn(Earning earning, EarningCategory category)
+        {
+            return earningDAL.InsertEarn(earning, category);
+        }
     }
 }
