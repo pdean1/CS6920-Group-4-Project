75b29605139ca9f3077c0faa0bd26a2af953ba61
twilli58 <twilli58@my.westga.edu>
2016-06-23 16:33:15 -0400

    Adding the test class for the manage Earnings Dal

---
+++ CS6920Group4ProjectTests/DALTests/ModelTests/ManageEarningsDALTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CS6920Group4Project.DAL.Model;
+using CS6920Group4Project.Model;
+using System.Collections.Generic;
+
+namespace CS6920Group4ProjectTests.DALTests.ModelTests
+{
+    class ManageEarningsDALTests
+    {
+
+    }
+}
