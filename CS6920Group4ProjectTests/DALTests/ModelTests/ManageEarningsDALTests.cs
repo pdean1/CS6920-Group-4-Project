8412f0a0ed276e09be246a10b5bb256391b17e45
twilli58 <twilli58@my.westga.edu>
2016-06-23 17:03:29 -0400

    updating with some changes to the test file

--- CS6920Group4ProjectTests/DALTests/ModelTests/ManageEarningsDALTests.cs
+++ CS6920Group4ProjectTests/DALTests/ModelTests/ManageEarningsDALTests.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CS6920Group4Project.DAL.Model;
 using CS6920Group4Project.Model;
 using System.Collections.Generic;
 
+
 namespace CS6920Group4ProjectTests.DALTests.ModelTests
 {
-    class ManageEarningsDALTests
+    [TestClass]
+    public class ManageEarningsDALTests
     {
+        
+        [TestMethod]
+        public void TestManageEarningsIsReturningTrue()
+        {
 
+        }
+       
     }
 }
