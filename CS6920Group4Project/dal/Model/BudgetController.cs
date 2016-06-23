777eee76ba634c722a3a2fbec3b390cf3461781c
pdean1 <pdean1@my.westga.edu>
2016-06-22 20:25:50 -0400

    Added the budget dal back to the project and added the path to the csproj file for some reason it went away?

---
+++ CS6920Group4Project/dal/Model/BudgetController.cs
@@ -0,0 +1,50 @@
+﻿using CS6920Group4Project.DAL.Model;
+using CS6920Group4Project.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS6920Group4Project.Controller
+{
+    /// <summary>
+    /// This class controls access to the model class Budget's DAL Methods
+    /// </summary>
+    class BudgetController
+    {
+        private static BudgetController instance;
+
+        private readonly BudgetDAL budgetDAL = new BudgetDAL();
+
+        private BudgetController() { }
+
+        public static BudgetController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BudgetController();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Calls the BudgetDAL class's GetAllUserBudgets Method to retrieve all budgets associated with a user.
+        /// If, however, ID is null or is less than or equal to zero this method will return null
+        /// </summary>
+        /// <param name="ID">ID of the User to retrieve budgets for</param>
+        /// <returns>A List of budgets or null in the case of invalid parameters being passed to the program</returns>
+        public List<Budget> GetUsersBudgets(int ID)
+        {
+            if (ID == null)
+                return null;
+            if (ID <= 0)
+                return null;
+            return budgetDAL.GetAllUserBudgets(ID);
+        }
+
+    }
+}
