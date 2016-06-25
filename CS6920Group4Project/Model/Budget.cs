3660005398ad6bf30e88dda77e13198d31173695
pdean1 <pdean1@my.westga.edu>
2016-06-25 00:30:57 -0400

    Added functions that get the total value of Bills, Earnings, and Expenses associated with a budget

--- CS6920Group4Project/Model/Budget.cs
+++ CS6920Group4Project/Model/Budget.cs
@@ -17,5 +17,35 @@ namespace CS6920Group4Project.Model
         public List<Bill> Bills { get; set; }
         public List<Earning> Earnings { get; set; }
         public List<Expense> Expenses { get; set; }
+
+        public decimal GetTotalAmountOfBills ()
+        {
+            decimal amount = 0;
+            foreach (Bill b in Bills)
+            {
+                amount += b.Amount;
+            }
+            return amount;
+        }
+        
+        public decimal GetTotalAmountOfEarnings()
+        {
+            decimal amount = 0;
+            foreach (Earning e in Earnings)
+            {
+                amount += e.Amount;
+            }
+            return amount;
+        }
+        
+        public decimal GetTotalAmountOfExpenses()
+        {
+            decimal amount = 0;
+            foreach (Expense e in Expenses)
+            {
+                amount += e.Amount;
+            }
+            return amount;
+        }
     }
 }
