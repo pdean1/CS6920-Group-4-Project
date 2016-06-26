c46f9431c27eb6253181c755d6875f225dffb691
iledelin1 <iedelin1@my.westga.edu>
2016-06-26 01:35:48 -0400

    Fix merger changes.  remove duplicate

--- CS6920Group4Project/Controller/BillController.cs
+++ CS6920Group4Project/Controller/BillController.cs
@@ -37,16 +37,10 @@ namespace CS6920Group4Project.Controller
         {
             return _dal.GetBillCategoryList();
         }
-<<<<<<< HEAD
 
         public long InsertBill(Bill bill)
         {
             return _dal.InsertBill(bill);
-=======
-        public long InsertBill(Bill b)
-        {
-            return _dal.InsertBill(b);
->>>>>>> 6ed44ef9f1209d253f30e2e97f440a0d9d23c46f
         }
     }
 }
