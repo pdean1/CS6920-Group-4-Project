42747e75d7614b4413c678bc0df2175d892587b8
pdean1 <pdean1@my.westga.edu>
2016-06-27 17:53:23 -0400

    Added a string utilities class

---
+++ CS6920Group4Project/Utilities/StringUtilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS6920Group4Project.Utilities
+{
+    public class StringUtilities
+    {
+        public static string GetDisplayableDollarAmount(decimal amount)
+        {
+            return string.Format("{0:c}", amount);
+        }
+    }
+}
