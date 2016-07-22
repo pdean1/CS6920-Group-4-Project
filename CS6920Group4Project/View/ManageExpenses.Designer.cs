8b623f9bd99dee6d1dc20058a6b8991270caa80d
iledelin1 <iedelin1@my.westga.edu>
2016-07-21 22:08:55 -0400

    Add dateTimePicker to datagridview so that user is able to edit spent and due dates

--- CS6920Group4Project/View/ManageExpenses.Designer.cs
+++ CS6920Group4Project/View/ManageExpenses.Designer.cs
@@ -167,6 +167,7 @@
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.Size = new System.Drawing.Size(932, 585);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label2
